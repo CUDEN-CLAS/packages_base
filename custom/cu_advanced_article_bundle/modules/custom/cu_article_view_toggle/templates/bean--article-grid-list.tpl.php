58156ae042adfb0ac98cba16b0d4295103c20419
kevincrafts <kevin.crafts@colorado.edu>
2016-07-11 12:06:41 -0600

    adding exposed filter, queries instead of entity references NEWS-1

--- custom/cu_advanced_article_bundle/modules/custom/cu_article_view_toggle/templates/bean--article-grid-list.tpl.php
+++ custom/cu_advanced_article_bundle/modules/custom/cu_article_view_toggle/templates/bean--article-grid-list.tpl.php
@@ -1,22 +1,32 @@
-<div id="article-toggle-<?php print $bean->bid; ?>">
-  <div class="article-toggle-navigation" aria-hidden="true">
-    <ul>
-      <li>
-        <a href="#article-toggle-grid-<?php print $bean->bid; ?>" class="article-toggle-grid-link" data-article-toggle-id="<?php print $bean->bid; ?>"><i class="fa fa-th" aria-hidden="true"></i> <span class="element-invisible">Grid</span></a>
-      </li>
-      <li>
-        <a href="#article-toggle-list-<?php print $bean->bid; ?>" class="article-toggle-list-link" data-article-toggle-id="<?php print $bean->bid; ?>"><i class="fa fa-align-justify" aria-hidden="true"></i> <span class="element-invisible">Grid</span></a>
-      </li>
-    </ul>
-  </div>
-  <div id="article-toggle-grid-<?php print $bean->bid; ?>" class="article-toggle-section article-toggle-grid">
+<div id="article-toggle-<?php print $bean->bid; ?>" class="article-grid-list">
+  <div class="article-grid-list-filter">
+    <div class="article-toggle-navigation" aria-hidden="true">
+      <ul>
+        <li>
+          <a href="#article-toggle-grid-<?php print $bean->bid; ?>" class="article-toggle-grid-link" data-article-toggle-id="<?php print $bean->bid; ?>"><i class="fa fa-th" aria-hidden="true"></i> <span class="element-invisible">Grid</span></a>
+        </li>
+        <li>
+          <a href="#article-toggle-list-<?php print $bean->bid; ?>" class="article-toggle-list-link" data-article-toggle-id="<?php print $bean->bid; ?>"><i class="fa fa-align-justify" aria-hidden="true"></i> <span class="element-invisible">Grid</span></a>
+        </li>
+      </ul>
+    </div>
     <?php
-      print render($content['field_article_view_grid']);
+      print render($content['exposed_filter']);
     ?>
+  </div>
+
+
+  <div id="article-toggle-grid-<?php print $bean->bid; ?>" class="article-toggle-section article-toggle-grid">
+    <div class="row clearfix">
+      <?php
+        print render($content['article_grid']);
+      ?>
+    </div>
+
   </div>
   <div id="article-toggle-list-<?php print $bean->bid; ?>" class="article-toggle-section article-toggle-list">
     <?php
-      print render($content['field_article_view_list']);
+      print render($content['article_list']);
     ?>
   </div>
 </div>
